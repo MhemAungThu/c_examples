fe9fccd16c96668234ba751d90286611e52c07ba
MhemAungThu <kweesai.saitama@gmail.com>
2021-01-20 19:00:48 +0630

    first commit

---
+++ cs:app/ch_02/reverse_array.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+
+void inplace_swap(int *x, int *y) {
+	*y = *x ^ *y;
+	*x = *x ^ *y;
+	*y = *x ^ *y;
+}
+
+// this code is already fixed the problem.
+void reverse_array(int a[], int cnt){
+	int first, last;
+
+	for (first = 0, last = cnt -1; first < last; first++, last--)
+		inplace_swap(&a[first], &a[last]);
+
+}
+
+int main(void) {
+	int a[5] = {1, 2, 3, 4, 5};
+	reverse_array(a, 5);
+
+	printf("[");
+	for (int i=0; i < 5; i++) {
+		printf("%d, ", a[i]);
+	}
+	printf("]\n");
+}
